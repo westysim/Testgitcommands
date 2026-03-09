5e3c1fac96cb6bd728fd961e62b9127ccd98c557
westsim <westrickdavid2008@gmail.com>
2026-03-09 08:05:56 -0500

    Added Print Statement

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 public class Main{
-  public static void Main(String[] args )
+  public static void main(String[] args )
   {
     System.out.println("Hello worlds");
+    System.out.println("test");
   }
 }
